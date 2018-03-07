e6462b9a3b955bac6d9617250f67ffdd1915831e
Zhenwei Shi <zhenweishi88@gmail.com>
2018-03-07 09:16:59 +0100

    O-RAW version-1 update

---
+++ PyrexReader.py
@@ -0,0 +1,196 @@
+"""
+###############################
+@author: zhenwei.shi, Maastro##
+###############################
+Usage:
+import PyrexReader
+	
+img_path = '.\CTscan'
+rtstruct_path = '.\RTstruct'
+ROI = Region Of Interest
+Img,Mask = PyrexReader.Img_Bimask(img_path,rtstruct_path,ROI)
+
+"""
+
+import dicom,os
+import numpy as np
+from skimage import draw
+import SimpleITK as sitk
+import re
+
+
+#class py_Reader:
+def match_ROIid(rtstruct_path,ROI_name):
+    mask_vol = Read_RTSTRUCT(rtstruct_path)
+    M= mask_vol[0]
+    for i in range(len(M.StructureSetROISequence)):
+        if str(ROI_name)==M.StructureSetROISequence[i].ROIName:
+            ROI_number = M.StructureSetROISequence[i].ROINumber
+#            print ROI_number
+            break
+    for ROI_id in range(len(M.StructureSetROISequence)):
+        if ROI_number == M.ROIContourSequence[ROI_id].RefdROINumber:
+#            print ROI_number
+            break
+    return ROI_id
+
+	
+def ROI_match(ROI,rtstruct_path):
+    mask_vol=Read_RTSTRUCT(rtstruct_path)
+    M=mask_vol[0]
+    target = []
+    for i in range(0,len(M.StructureSetROISequence)):
+        if re.search(ROI,M.StructureSetROISequence[i].ROIName):
+            target.append(M.StructureSetROISequence[i].ROIName)		
+    if len(target)==0:
+        for j in range(0,len(M.StructureSetROISequence)):
+            print M.StructureSetROISequence[j].ROIName
+            break
+        print 'Input ROI is: '
+        ROI_name = raw_input()
+        target.append(ROI_name)
+    print '------------------------------------'
+    return target
+
+# Read scans under the specified path 
+def Read_scan(path):
+    scan = [dicom.read_file(os.path.join(path, s), force=True) for s in os.listdir(path)]
+    try:
+        scan.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+    except:
+        print "AttributeError: Cannot read scans'"
+    return scan
+# Read RTSTRUCT under the specified path
+def Read_RTSTRUCT(path):
+    try:
+        rt = [dicom.read_file(os.path.join(path, s), force=True)for s in os.listdir(path)]
+    except:
+        print "AttributeError: Cannot read RTSTRUCT'"
+    return rt
+
+# Mask interpolation
+def poly2mask(vertex_row_coords, vertex_col_coords, shape):
+    fill_row_coords, fill_col_coords = draw.polygon(vertex_row_coords, vertex_col_coords, shape)
+    mask = np.zeros(shape, dtype=np.bool)
+    mask[fill_row_coords, fill_col_coords] = True
+    return mask
+
+# Units to Hounsfield Unit (HU) by multiplying rescale slope and add intercept
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16) #convert to int16
+    # the code below checks if the image has slope and intercept
+    # since MRI images often do not provide these
+    try:
+        intercept = scans[0].RescaleIntercept
+        slope = scans[0].RescaleSlope
+    except AttributeError:
+        pass
+    else:
+        if slope != 1:
+            image = slope * image.astype(np.float64)
+            image = image.astype(np.int16)        
+        image += np.int16(intercept)    
+    return np.array(image, dtype=np.int16)
+
+# generating image array and binary mask
+def Img_Bimask(img_path,rtstruct_path,ROI_name):
+    img_vol = Read_scan(img_path)
+    mask_vol=Read_RTSTRUCT(rtstruct_path)
+	# Slices usually have the same basic information including slice size, patient position, etc.
+    IM=img_vol[0]
+    IM_P=get_pixels_hu(img_vol)
+    M=mask_vol[0]
+    num_slice=len(img_vol)
+    mask=np.zeros([num_slice, IM.Rows, IM.Columns],dtype=np.uint8)
+    xres=np.array(IM.PixelSpacing[0])
+    yres=np.array(IM.PixelSpacing[1])
+    ROI_id = match_ROIid(rtstruct_path,ROI_name)
+
+#Check DICOM file Modality
+    if IM.Modality == 'CT' or 'PT':
+		for k in range(len(M.ROIContourSequence[ROI_id].ContourSequence)):    
+			Cpostion_rt = M.ROIContourSequence[ROI_id].ContourSequence[k].ContourData[2]
+			
+			for i in range(num_slice):
+				if Cpostion_rt == img_vol[i].ImagePositionPatient[2]:
+					sliceOK = i
+					break
+					
+			x=[]
+			y=[]
+			z=[]
+			
+			m=M.ROIContourSequence[ROI_id].ContourSequence[k].ContourData
+                         
+    elif IM.Modality == 'MR':
+        slice_0 = img_vol[0]
+        slice_n = img_vol[-1]
+
+		# the screen coordinates, including the slice number can then be computed 
+		# using the inverse of this matrix
+        transform_matrix = np.r_[slice_0.ImageOrientationPatient[3:], 0, slice_0.ImageOrientationPatient[:3], 0, 0, 0, 0, 0, 1, 1, 1, 1].reshape(4, 4).T # yeah that's ugly but I didn't have enough time to make anything nicer
+        T_0 = np.array(slice_0.ImagePositionPatient)
+        T_n = np.array(slice_n.ImagePositionPatient)
+        col_2 = (T_0 - T_n) / (1 - len(img_vol))
+        pix_s = slice_0.PixelSpacing
+        transform_matrix[:, -1] = np.r_[T_0, 1] 
+        transform_matrix[:, 2] = np.r_[col_2, 0] 
+        transform_matrix[:, 0] *= pix_s[1]
+        transform_matrix[:, 1] *= pix_s[0]
+        
+        transform_matrix = np.linalg.inv(transform_matrix)
+        for s in M.ROIContourSequence[ROI_id].ContourSequence:    
+			Cpostion_rt = np.r_[s.ContourData[:3], 1] # the ROI point to get slice number from
+													  # in homogenous coordinates
+
+			roi_slice_nb = int(transform_matrix.dot(Cpostion_rt)[2]) # the slice number according to the 
+																	 # inverse transform
+			for i in range(num_slice):
+				print(roi_slice_nb, i)
+				if roi_slice_nb == i:
+					sliceOK = i
+					break
+			x=[]
+			y=[]
+			z=[]			                            
+			m=s.ContourData
+            
+    for i in range(0,len(m),3):
+		x.append(m[i+1])
+		y.append(m[i+0])
+		z.append(m[i+2])
+	
+    x=np.array(x)
+    y=np.array(y)
+    z=np.array(z)
+    
+    x-= IM.ImagePositionPatient[1]
+    y-= IM.ImagePositionPatient[0]
+    z-= IM.ImagePositionPatient[2]
+    pts = np.zeros([len(x),3])	
+    pts[:,0] = x
+    pts[:,1] = y
+    pts[:,2] = z
+    a=0
+    b=1
+    p1 = xres
+    p2 = yres
+    m=np.zeros([2,2])             
+    m[0,0]=img_vol[sliceOK].ImageOrientationPatient[a]*p1 
+    m[0,1]=img_vol[sliceOK].ImageOrientationPatient[a+3]*p2
+    m[1,0]=img_vol[sliceOK].ImageOrientationPatient[b]*p1
+    m[1,1]=img_vol[sliceOK].ImageOrientationPatient[b+3]*p2
+     
+	# Transform points from reference frame to image coordinates
+    m_inv=np.linalg.inv(m)
+    pts = (np.matmul((m_inv),(pts[:,[a,b]]).T)).T
+    mask[sliceOK,:,:] = np.logical_or(mask[sliceOK,:,:],poly2mask(pts[:,0],pts[:,1],[IM_P.shape[1],IM_P.shape[2]]))
+    IM_P-=np.min(IM_P)
+    IM_P=IM_P.astype(np.float32)
+    IM_P/=np.max(IM_P)
+    IM_P*=255
+    
+    Img=sitk.GetImageFromArray(IM_P.astype(np.float32))
+    Mask=sitk.GetImageFromArray(mask)
+    return Img, Mask
---
+++ README.md
@@ -0,0 +1,42 @@
+## Py-rex (Pyradiomics extension) Version 1.3
+
+This is an open-source python package for [pyradiomics](https://github.com/Radiomics/pyradiomics) extension on both input and output sides.
+With this pacakge we aim to allow users to use origial DICOM file and RTSTRUCT for radiomics calculation. 
+
+### Features
+1. Py-rex is allowed users employ original DICOM files and RTstruture;
+2. Internal module for creation of ROI binary mask;
+3. Computation of radiomic features on a specific ROI;
+4. Semi-automatic approach to handle multiple ROI names;
+5. Radiomic features output in different formats (e.g., rdf, text and csv) with related ontologies (e.g., [Radiomics Ontology](https://bioportal.bioontology.org/ontologies/ROO) and [Radiation Oncology Ontology](https://bioportal.bioontology.org/ontologies/RO)).
+6. Applicable for CT and MRI.
+7. Allow batch processing.
+
+### Using Py-rex
+
+1. Install [pyradiomics](https://github.com/Radiomics/pyradiomics)
+2. Clone/Download Py-rex to the sub-directory of pyardiomics-master
+3. Execute: `python -m pip install -r pyrex_requirements.txt`
+4. Fill in configuration file "./pyradiomics-master/Py-rex-master/ParamsSettings/Pyrex_Params.yaml"
+5. Execute `HelloPyrex.py`
+6. Results in "./RFstore"
+
+		
+### Example
+We provide a test dataset in `./data`. This dataset has a series of CT/MRI scans of a lung cancer patient and the corresponding RTSTRUCT.
+
+```
+python ./HelloPyrex.py
+```
+### License
+
+Py-rex may not be used for commercial purposes. This package is freely available to browse, download, and use for scientific 
+and educational purposes as outlined in the [Creative Commons Attribution 3.0 Unported License](https://creativecommons.org/licenses/by/3.0/).
+
+### Developers
+ - [Zhenwei Shi](https://github.com/zhenweishi)<sup>1</sup>
+ - [Leonard Wee]<sup>1</sup>
+ - [Andre Dekker]<sup>1</sup>
+ 
+<sup>1</sup>Department of Radiation Oncology (MAASTRO Clinic), GROW – School for Oncology and Development Biology, Maastricht University Medical Centre, The Netherlands.
+
